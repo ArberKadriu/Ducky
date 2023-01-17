b83c571b4a02ab9ae132e8ac1538776622786464
Arber Kadriu <arber_kadriu@yahoo.com>
2023-01-16 23:25:39 -0500

    update server url

--- server/server.js
+++ server/server.js
@@ -9,7 +9,6 @@ const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-console.log('OpenAI API Key:', process.env.OPENAI_API_KEY)
 const openai = new OpenAIApi(configuration);
 
 const app = express()
@@ -18,7 +17,7 @@ app.use(express.json())
 
 app.get('/', async (req, res) => {
   res.status(200).send({
-    message: 'Hello from Ducky!'
+    message: 'Hello from CodeX!'
   })
 })
 
